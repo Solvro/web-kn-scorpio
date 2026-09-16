3a37c9db701781a5516948bd717802a7b47f04a3
olios123 <oliwier4444k@gmail.com>
2026-09-16 23:25:02 +0200

    feat: create file to store temporary data

---
+++ src/app/contact/temporary-data.ts
@@ -0,0 +1,113 @@
+const CONTACT_DEPARTMENTS = [
+  {
+    role: "Lider Zespołu & Kontakt Ogólny",
+    name: "Zarząd KN SCORPIO",
+    email: "email@pwr.edu.pl",
+    description: "Pytania ogólne, sprawy organizacyjne i naukowe.",
+  },
+  {
+    role: "Marketing & Sponsoring",
+    name: "Dział PR i Współpracy",
+    email: "email@pwr.edu.pl",
+    description: "Współpraca biznesowa, sponsoring i media.",
+  },
+  {
+    role: "Rekrutacja & Dołączenie do Koła",
+    name: "Dział HR",
+    email: "email@pwr.edu.pl",
+    description: "Pytania dotyczące dołączenia do zespołu łazika.",
+  },
+];
+
+const BOARD_MEMBERS = [
+  {
+    name: "Dominik Pawliszewski",
+    role: "Prezes Koła",
+    phone: "+48 000 000 000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    src: "/team/Dominik-Pawilszewski.jpg",
+  },
+  {
+    name: "Kinga Adamska",
+    role: "Wiceprezeska Koła",
+    phone: "+48 000 000 000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    src: "/team/Kinga_Adamska.jpg",
+  },
+  {
+    name: "Krzysztof Piechota",
+    role: "Program Director",
+    phone: "+48 000 000 000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    src: "/team/Krysia_Fluder.jpg",
+  },
+  {
+    name: "Krysia Fluder",
+    role: "Liderka działu marketingu",
+    phone: "+48 000 000 000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    src: "/team/Krzysztof-Piechota.jpg",
+  },
+  {
+    name: "Bartłomiej Łuczeńczyk",
+    role: "Lider działu konstrukcji",
+    phone: "+48 000 000 000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    src: "/team/Bartlomiej_Luczenczyk.jpg",
+  },
+  {
+    name: "Jakub Górski",
+    role: "Lider działu elektroniki",
+    phone: "+48 000 000 000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    src: "/team/Gorski_J.jpg",
+  },
+  {
+    name: "Igor Zaworski",
+    role: "Lider działu Software",
+    phone: "+48 000 000 000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    src: "/team/Igor-Zaworski.jpg",
+  },
+  {
+    name: "Aleksandra Korytkowska",
+    role: "Liderka działu Science",
+    phone: "+48 000 000 000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    src: "/team/Ola_Korytkowska.jpg",
+  },
+];
+
+const ACADEMIC_ADVISORS = [
+  {
+    name: "Dr inż. Adrian Zakrzewski",
+    email: "email@pwr.edu.pl",
+    phone: "+48 000 000 000",
+    web: "https://pwr.edu.pl",
+    src: "/advisors/Adrian.jpg",
+  },
+  {
+    name: "Dr hab inż. Maciej Zwierzchowski",
+    email: "email@pwr.edu.pl",
+    phone: "+48 000 000 000",
+    web: "https://pwr.edu.pl",
+    src: "/advisors/Maciej.jpg",
+  },
+  {
+    name: "Dr inż. Jakub Chołodowski",
+    email: "email@pwr.edu.pl",
+    phone: "+48 000 000 000",
+    web: "https://pwr.edu.pl",
+    src: "/advisors/Jakub.jpg",
+  },
+];
+
+export { BOARD_MEMBERS, ACADEMIC_ADVISORS, CONTACT_DEPARTMENTS };
